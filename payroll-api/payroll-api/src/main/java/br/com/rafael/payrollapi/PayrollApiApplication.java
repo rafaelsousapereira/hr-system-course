6ece7bdb7fac7afcd68bd22776c14c8ce5c6c648
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-08-29 16:03:29 -0300

    feat: eureka server created

--- payroll-api/payroll-api/src/main/java/br/com/rafael/payrollapi/PayrollApiApplication.java
+++ payroll-api/payroll-api/src/main/java/br/com/rafael/payrollapi/PayrollApiApplication.java
@@ -2,10 +2,12 @@ package br.com.rafael.payrollapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class PayrollApiApplication {
 
     public static void main(String[] args) {
